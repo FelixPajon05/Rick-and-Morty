c0ff07993f4705557c83212e6c6ce93a46a382e0
FelixPajon05 <felixpajon05@gmail.com>
2023-04-24 01:57:14 -0600

    The rick and morty API is consumed with javascript, the view is created in html and styles are applied with css.

---
+++ js/index.js
@@ -0,0 +1,47 @@
+function getAllCharacters(characters) {
+    
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    
+    results
+        .then(res => res.json())
+        .then(data => {
+            characters(data)
+    });
+}
+
+function getCharacter(characters) {
+    
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    
+    results
+        .then(res => res.json())
+        .then(data => {
+            characters(data)
+    });
+}
+
+getAllCharacters(data => {
+
+    data.results.forEach(personaje => {
+
+        const article = document.createRange().createContextualFragment(/*html*/`
+
+        <article>
+
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+
+            <h2>${personaje.name}</h2>
+            <span>${personaje.status}</span>
+
+        </article>
+
+        `);
+
+        const main = document.querySelector("main");
+
+        main.append(article);
+
+    });
+});
